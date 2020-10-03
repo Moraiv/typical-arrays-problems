591164096a4876b358a0f789067def3ae7221484
Moraiv <47919247+Moraiv@users.noreply.github.com>
2020-10-03 22:54:09 +0400

    feat: add functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === undefined  || array.length === 0) {
+return 0
+  }
+  else
+  return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+   if (typeof array === undefined  || array.length === 0) {
+return 0
+  }
+  else
+return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+     if (typeof array === undefined  || array.length === 0) {
+return 0
+  }
+  else
+ var avgNumber = 0
+ for (let x = 0; x < array.length; x++) {
+ avgNumber += array[x]
+ }
+avgNumber = avgNumber/array.length
+   return avgNumber
 }
