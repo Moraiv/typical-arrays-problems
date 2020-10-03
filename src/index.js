52fd84e12301c2a7ebbab2b382b7f53c52151e0b
Moraiv <47919247+Moraiv@users.noreply.github.com>
2020-10-03 22:58:08 +0400

    refactor: changing the code structure

--- src/index.js
+++ src/index.js
@@ -1,29 +1,20 @@
 
 exports.min = function min (array) {
-  if (typeof array === undefined  || array.length === 0) {
-return 0
-  }
-  else
-  return Math.min.apply(null, array)
+  if (typeof array === undefined  || array.length === 0) return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-   if (typeof array === undefined  || array.length === 0) {
-return 0
-  }
-  else
-return Math.max.apply(null, array)
+  if (typeof array === undefined  || array.length === 0) return 0;
+return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-     if (typeof array === undefined  || array.length === 0) {
-return 0
-  }
-  else
- var avgNumber = 0
+  if (typeof array === undefined  || array.length === 0) return 0;
+ var avgNumber = 0;
  for (let x = 0; x < array.length; x++) {
  avgNumber += array[x]
  }
 avgNumber = avgNumber/array.length
-   return avgNumber
+   return avgNumber;
 }
